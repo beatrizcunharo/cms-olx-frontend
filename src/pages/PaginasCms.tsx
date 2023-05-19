ac67f170c4dccf92c3ef4fa71d2d7dbd86755e90
beatrizcunharo <beatrizcunharo@gmail.com>
2023-05-18 23:46:07 -0300

    feat: Adicionando responsividade ao componente do input buscar e botão criar novo

--- src/pages/PaginasCms.tsx
+++ src/pages/PaginasCms.tsx
@@ -1,11 +1,11 @@
 import { BotaoCms } from '../components/atom/BotaoCms'
-import { ChevronDownIcon, MagnifyingGlassIcon } from '@heroicons/react/24/solid'
+import { MagnifyingGlassIcon } from '@heroicons/react/24/solid'
 import '../styles/paginas.scss'
 
 export const PaginasCms = () => {
     return (
-        <section className="cmsPages">
-            <div className='container'>
+        <section className='mainSectionPages'>
+            <div className="container cmsPages">
                 <span className='title'>Páginas</span>
                 <div className='boxPages'>
                     <div className='firstDiv'>
@@ -13,7 +13,7 @@ export const PaginasCms = () => {
                             <input placeholder='Buscar' className='inputDiv-input' />
                             <button className='inputDiv-buttonIcon'><MagnifyingGlassIcon width={15} height={15} className='icon' /></button>
                         </div>
-                        <BotaoCms texto='Criar Novo' cor='roxo' style='buttonNewPage' onClick={() => console.log("teste")}>Criar Novo<ChevronDownIcon width={18} height={18} className='buttonNewPage-icon' /></BotaoCms>
+                        <BotaoCms texto='Criar Novo' cor='roxo' style='buttonNewPage' onClick={() => console.log("cliquei")}>Criar Novo</BotaoCms>
                     </div>
                 </div>
             </div>
